c081353cabf398541790a130824a586d69bdf952
nentu <zajtsev.artjom@samlit.net>
2022-10-13 11:07:00 +0300

    1 pokemon

--- org/itmo/Main.java
+++ org/itmo/Main.java
@@ -1,14 +1,15 @@
 package org.itmo;
 
+import org.itmo.pokemons.Guzzlord;
 import ru.ifmo.se.pokemon.*;
 
 public class Main {
     public static void main(String[] args){
         Battle b = new Battle();
-        Pokemon p1 = new Pokemon("Artem1", 1);
-        Pokemon p2 = new Pokemon("Artem2", 1);
-        b.addAlly(p1);
-        b.addFoe(p2);
+
+
+        b.addAlly(new Guzzlord("Guzzlord1", 1));
+        b.addFoe(new Pokemon("sdfs", 3));
 
         b.go();
 
