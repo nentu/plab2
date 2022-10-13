2659b10a3b27585de9684f8b938da0c1dd1c01c9
nentu <zajtsev.artjom@samlit.net>
2022-10-13 23:46:05 +0300

    Complited

--- org/itmo/attacks/guzzlord/RazorLeaf.java
+++ org/itmo/attacks/guzzlord/RazorLeaf.java
@@ -7,9 +7,9 @@ public class RazorLeaf extends PhysicalMove {
         super(Type.GRASS, 55.0, 95.0);
     }
 
+    @Override
     public double calcCriticalHit(Pokemon var1, Pokemon var2) {
         if (var1.getStat(Stat.SPEED)*3 / 512.0 > Math.random()) {
-            System.out.println("Критический удар!");
             return 2.0;
         } else {
             return 1.0;
